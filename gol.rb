f5e82b8c47eca0046079091b9c3aaa8dfa43e6a1
Jikku Jose <jikkujose@gmail.com>
2016-03-23 15:09:35 +0530

    Add GoL algorithm

---
+++ gol.rb
@@ -0,0 +1,52 @@
+module GameOfLife
+  class World
+    attr_reader :state
+
+    def initialize(hash)
+      @state = hash
+      @state.default = false
+
+      @next_state = {}
+      @next_state.default = false
+    end
+
+    def next
+      @state.each do |o_coord, o_state|
+        (surroundings(o_coord) + [o_coord]).each do |n_coord|
+          computed_state = case neighbour_count(n_coord)
+                           when 2
+                             @state[n_coord]
+                           when 3
+                             true
+                           else
+                             false
+                           end
+
+          @next_state[n_coord] = computed_state if computed_state
+        end
+      end
+
+      self.class.new(@next_state)
+    end
+
+    def neighbour_count(coordinate)
+      surroundings(coordinate).count do |c|
+        @state[c]
+      end
+    end
+
+    def surroundings(coordinate)
+      grid_mask.map do |left, right|
+        [coordinate[0] + left, coordinate[1] + right]
+      end
+    end
+
+    def grid_mask
+      [-1, 0, 1].product([-1, 0, 1]) - [[0, 0]]
+    end
+
+    def ==(other)
+      @state == other.state
+    end
+  end
+end
---
+++ tests/test_gol.rb
@@ -0,0 +1,71 @@
+require_relative './test_helper.rb'
+require_relative '../gol.rb'
+
+class GoLTest < Minitest::Test
+  def setup
+    @g = GameOfLife::World
+  end
+
+  def test_empty
+    hash = {}
+    assert_equal @g.new(hash), @g.new(hash).next
+  end
+
+  def test_single
+    hash = {[23, 4] => true}
+    assert_equal @g.new({}), @g.new(hash).next
+  end
+
+  def test_square
+    hash = {
+      [0, 0] => true,
+      [0, 1] => true,
+      [1, 0] => true,
+      [1, 1] => true
+    }
+
+    assert_equal @g.new(hash), @g.new(hash).next
+  end
+
+  def test_cross_repeater
+    repeat_0 = {
+      [0, 0] => true,
+      [0, 1] => true,
+      [0, 2] => true,
+    }
+
+    repeat_1 = {
+      [-1, 1] => true,
+      [0, 1] => true,
+      [1, 1] => true,
+    }
+
+    assert_equal @g.new(repeat_0), @g.new(repeat_1).next
+  end
+
+  def test_beacon
+    repeat_0 = {
+      [0, 0] => true,
+      [0, 1] => true,
+      [1, 0] => true,
+      [1, 1] => true,
+      [2, 2] => true,
+      [2, 3] => true,
+      [3, 2] => true,
+      [3, 3] => true,
+    }
+
+    repeat_1 = {
+      [0, 0] => true,
+      [0, 1] => true,
+      [1, 0] => true,
+      [2, 3] => true,
+      [3, 2] => true,
+      [3, 3] => true,
+    }
+
+    assert_equal @g.new(repeat_0), @g.new(repeat_1).next
+  end
+end
+
+Minitest.autorun
